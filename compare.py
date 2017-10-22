fe55ab91ee0afc37509f66a0012c6c0752f0633b
Evan <me@thaske.com>
2017-10-22 17:00:22 -0400

    Refactored

---
+++ compare.py
@@ -0,0 +1,47 @@
+from scipy.signal import hilbert
+from scipy.io import wavfile
+import numpy as np
+import scipy
+
+def compare(file1, file2):
+	snd1 = read_file(file1)
+	snd2 = read_file(file2)
+	snd1 = resize(snd1)
+	snd2 = resize(snd2)
+	env1 = amp_env(snd1)
+	env2 = amp_env(snd2)
+	percentage = correlate(env1, env2)
+	return percentage
+
+def read_file(file):
+	sampFreq, sound = wavfile.read(file)											# Read the file
+	snd = sound[:,0]																# Choose one channel of audio to compare
+	return snd
+
+def resize2(snd1, snd2):
+	if len(snd1) != len(snd2):														# Make arrays the same length
+		if len(snd1) > len(snd2):													# Determine which array is bigger
+			size_diff = len(snd1) - len(snd2)										# Determine how much bigger
+			snd2 = np.pad(snd2, (0,size_diff), 'constant', constant_values=(0,0)) 	# Pad with appropriate number of zeros
+			return (snd1, snd2)
+		elif len(snd1) < len(snd2):
+			size_diff = len(snd2) - len(snd1)										# Determine how much bigger
+			snd1 = np.pad(snd1, (0,size_diff), 'constant', constant_values=(0,0))	# Pad with appropriate number of zeros
+			return (snd1, snd2)
+
+def resize(snd):
+	if len(snd) < 176400:															# Clip the sample to 5 seconds
+		size_diff = 176400 - len(snd)												# Determine how much bigger
+		snd = np.pad(snd, (0,size_diff), 'constant', constant_values=(0,0))			# Pad with appropriate number of zeros
+	elif len(snd) > 176400:
+		snd = snd[:176401]
+	return snd
+
+def amp_env(snd):
+	amp_env = abs(hilbert(snd))														# Find the amplitude envelope
+	return amp_env
+
+def correlate(env1, env2):
+	corr = np.corrcoef(env1, env2)[0, 1]											# Find the normalized correlation of the envelopes
+	percent = round(corr*100,2)														# Round to get percentage
+	return percent
--- samplr.py
+++ samplr.py
@@ -1,67 +1,13 @@
-from scipy.signal import hilbert
-from scipy.io import wavfile
-import numpy as np
-import scipy
+from compare import *
 import sys
 import os
 
-
 def main(orig, directory):
-
 	print("Comparing {}".format(orig))
-
-	# Loop through folder to compare
-	for filename in os.listdir(directory):
-
-	    # Filter out only .wav files
-	    if filename.endswith(".wav"): 
-
-	    	# Get the path name to the files
-	    	file = '{}/{}'.format(directory,filename)
-
-	    	# Print the percentages
-	    	print("{}: {}%".format(filename, compare(orig, file)))
-
-
-def compare(file1, file2):
-
-	# Read the files
-	sampFreq1, sound1 = wavfile.read(file1)
-	sampFreq2, sound2 = wavfile.read(file2)
-
-	# Choose one channel of audio to compare
-	snd1 = sound1[:,0]
-	snd2 = sound2[:,0]
-
-	# Make arrays the same length
-	if len(snd1) != len(snd2):
-
-		# Determine which array is bigger
-		if len(snd1) > len(snd2):
-
-			# Determine how much bigger
-			size_diff = len(snd1) - len(snd2)
-			# Pad with appropriate number of zeros
-			snd2 = np.pad(snd2, (0,size_diff), 'constant', constant_values=(0,0))
-
-		elif len(snd1) < len(snd2):
-
-			# Determine how much bigger
-			size_diff = len(snd2) - len(snd1)
-			# Pad with appropriate number of zeros
-			snd1 = np.pad(snd1, (0,size_diff), 'constant', constant_values=(0,0))
-
-	# Find the amplitude envelope
-	amp_env1 = abs(hilbert(snd1))
-	amp_env2 = abs(hilbert(snd2))
-
-	# Correlate the envelopes
-	corr = np.corrcoef(amp_env1, amp_env2)[0, 1]
-
-	# Round to get percentage
-	percent = round(corr*100,2)	
-	return percent
-
+	for filename in os.listdir(directory):							# Loop through folder to compare
+	    if filename.endswith(".wav"): 								# Filter out only .wav files
+	    	file = '{}/{}'.format(directory,filename)				# Get the path name to the files
+	    	print("{}: {}%".format(filename, compare(orig, file)))	# Print the percentages
 
 if __name__ == "__main__":
 	main(sys.argv[1], sys.argv[2])
