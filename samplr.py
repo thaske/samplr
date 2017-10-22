747cf077e967995371b1e613884310e4db64c9f9
Evan <me@thaske.com>
2017-10-22 14:06:17 -0400

    Commit 2

--- samplr.py
+++ samplr.py
@@ -1,10 +1,14 @@
-from scipy.signal import hilbert
-from scipy.io import wavfile
-import numpy as np
-import scipy
-sampFreq1, snd1 = wavfile.read('clap.wav')
-sampFreq2, snd2 = wavfile.read('clap2.wav')
-amp_env1 = abs(hilbert(snd1[:, 0]))
-amp_env2 = abs(hilbert(snd2[:, 0]))
-corr = np.corrcoef(amp_env1, amp_env2)[0, 1]
-print('Similarity: {:.2f}%'.format(corr*100))
+from compare import *
+import os
+
+# compare('clap.wav', 'snare.wav')
+
+def main(orig, directory):
+	print("Comparing {}".format(orig))
+	for filename in os.listdir(directory):
+	    if filename.endswith(".wav"): 
+	    	file = '{}/{}'.format(directory,filename)
+	    	print("{}: {}%".format(filename, compare(orig, file)))
+
+if __name__ == "__main__":
+	main(sys.argv[1], sys.argv[2])
